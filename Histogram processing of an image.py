f16fe00185f649c556dbe2f3e43ee61df324e55a
Prajeeth <120513885+Prajeeth17@users.noreply.github.com>
2024-09-30 15:32:04 +0530

    Update Histogram processing of an image.py

--- Histogram processing of an image.py
+++ Histogram processing of an image.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 gray_image = cv2.imread("gray image of flower.jpg")
 color_image = cv2.imread()
 cv2.imshow("Gray Image",gray_image)
-
+cv2.imshow("Colour Image",color_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
@@ -21,14 +21,27 @@ import matplotlib.pyplot as plt
 grayscale_image=cv2.imread("gray image of flower.jpg")
 colourscale_image=cv2.imread("color image of flower.jpg")
 hist=cv2.calcHist(grayscale_image,[0],None,[255],[0,255])
-hist1=cv2.calcHist()
+hist1=cv2.calcHist([color_image],[1],None,[256],[0,256])
 plt.figure()
 plt.title("Histogram")
 plt.xlabel("")
 plt.ylabel("pixel count")
 plt.stem()
 plt.show()
-
+hist = cv2.calcHist([gray_image],[0],None,[256],[0,256])
+hist1 = cv2.calcHist([color_image],[1],None,[256],[0,256])
+plt.figure()
+plt.title("Histogram")
+plt.xlabel('GrayScaleValue')
+plt.ylabel('PixelCount')
+plt.stem(hist)
+plt.show()
+plt.figure()
+plt.title("Histogram")
+plt.xlabel('Intensity Value')
+plt.ylabel('PixelCount')
+plt.stem(hist1)
+plt.show()
 
 
 # In[3]:Write the code to perform histogram equalization of the image. 
@@ -41,7 +54,10 @@ gi=cv2.imread("gray image of flower.jpg",0)
 colorscale=cv2.imread("color image of flower.jpg")
 g=cv2.resize(gi,(500,400))
 equ=cv2.equalizeHist(gi)
-
+cv2.imshow('Gray Image',Gray_image)
+cv2.imshow('Equalized Image',equ)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
 
